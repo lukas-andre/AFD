58aaa6c1bb5c6626d87a99d647b1062cf48bb2c9
lukas-andre <lhenry@icci.cl>
2017-01-18 23:10:41 -0300

    change errors

--- Tm5.py
+++ Tm5.py
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 
 class Tm5(object): # tmf = transiciones multiplos de 5
 
     def __init__(self):
         self.Q = [0,1,2,3,4]
-        self.alfabeto = [0,1]
-=======
-class Tm5(object):
-
-    # tmf = transiciones multiplos de 5
-
-    def __init__(self):
-        self.Q = [0,1,2,3,4]
         self.Alfabeto = [0,1]
->>>>>>> 55649a28d70e5fa4b0cd21ea2631325c9e983801
         self.transiciones = {
                         (0,0):0,
                         (0,1):1,
@@ -28,14 +18,3 @@ class Tm5(object):
                         }
         self.q0 = 0
         self.qF = 0
-<<<<<<< HEAD
-
-    def get_data_type(self): # if has one or more str datatype return str
-        for i in self.alfabeto:
-            if type(i) == int:
-                _type = 'int'
-            if type(i) == str:
-                return type(i)
-        return _type
-=======
->>>>>>> 55649a28d70e5fa4b0cd21ea2631325c9e983801
--- main.py
+++ main.py
@@ -4,17 +4,6 @@ from Af import *
 
 x = 0
 tm5 = Tm5() # Mapa del automata.
-<<<<<<< HEAD
-print tm5.get_data_type()
-while x != 1 :
-    automata = Af( tm5.Q, tm5.alfabeto, tm5.transiciones, tm5.q0, tm5.qF )
-    palabra = raw_input("Ingrese palabra :")
-
-    if automata.validate(palabra,tm5.get_data_type()):
-        print automata.is_in(palabra,tm5.q0)
-    else:
-        print "No se reconoce lenguaje le recomendamos usar palabras este Alfabeto:" + str(tm5.alfabeto)
-=======
 while x != 1 :
     automata = Af( tm5.Q, tm5.Alfabeto, tm5.transiciones, tm5.q0, tm5.qF )
     palabra = raw_input("Inegrese palabra :")
@@ -22,4 +11,3 @@ while x != 1 :
         print automata.is_in(palabra,tm5.q0)
     else:
         print "No se reconoce lenguaje le recomendamos usar palabras este Alfabeto:" + str(tm5.Alfabeto)
->>>>>>> 55649a28d70e5fa4b0cd21ea2631325c9e983801
